d3d6591be2abdf3da70b77a6ef7d00041d1a866b
SwapKul <47084735+SwapKul@users.noreply.github.com>
2020-05-13 13:56:10 +0530

    Add files via upload

---
+++ milestone_project_2/by_json/utils/database.py
@@ -0,0 +1,49 @@
+import json
+
+
+books_file = 'books.json'
+
+
+def create_book_table():
+    try:
+        with open(books_file, "x") as file:
+            json.dump([], file)
+    except FileExistsError:
+        pass
+
+
+def get_all_books():
+    with open(books_file, "r") as file:
+        return json.load(file)
+
+
+def add_book(name, author):
+    books = get_all_books()
+    books.append({"name": name, "author": author, 'read': False})
+    _save_all_books(books)
+
+
+def _save_all_books(books):
+    with open(books_file, 'w') as file:
+        json.dump(books, file)
+
+
+def find_book(name):
+    books = get_all_books()
+    for book in books:
+        if book["name"] == name:
+            print(f'{book["name"]} by {book["author"]}.')
+
+
+def mark_as_read(name):
+    books = get_all_books()
+    for book in books:
+        if book["name"] == name:
+            book["read"] = True
+    _save_all_books(books)
+
+
+def delete_book(name):
+    books = get_all_books()
+    books = [book for book in books if book["name"] != name]
+    _save_all_books(books)
